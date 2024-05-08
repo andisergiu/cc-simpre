3fb2fca049b40961522ac81d8b7728111a20f46b
andisergiu <72200704+andisergiu@users.noreply.github.com>
2024-05-09 01:06:36 +0300

    A03

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('./models/User');
+const User = require('../models/User');
 
 // Ruta pentru înregistrarea unui utilizator
 router.post('/register', async (req, res) => {
